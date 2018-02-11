c5321559d4f2db366275cefcbd1f4b883ebe855b
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-02-11 10:05:33 +0900

    user register

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+          user: {
+              name: '',
+              email: 'user@invalid.com',
+              password: 'foo',
+              password_confirm: 'foo',
+          },
+      }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+          user: {
+              name: 'test',
+              email: 'test@valid.com',
+              password: 'hogehoge',
+              password_confirm: 'hogehoge',
+          },
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_equal flash[:success], 'ようこそ！Railsチュートリアルへ！'
+  end
+end
