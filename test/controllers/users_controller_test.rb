1e59ba764a4b405744ac94539816fd71f0e17163
imuuzak <kazuumi.hori@spacemarket.co.jp>
2018-02-12 15:30:29 +0900

    fix users RESTful

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -47,4 +47,24 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to signin_url
+  end
+
+  test "should redirect destroy when not signed in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to signin_url
+  end
+
+  test "should redirect destroy when signed in as non-admin" do
+    sign_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
 end
