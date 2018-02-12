34c3e6ef163d6131ef77e118efbdb1d146410f17
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-02-12 20:43:18 +0900

    dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'app87915562@heroku.com'
-  config.action_mailer.default_url_options = { host: host }
+  host = 'murmuring-chamber-36962.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   ActionMailer::Base.smtp_settings = {
       :address => 'smtp.sendgrid.net',
       :port => '587',
